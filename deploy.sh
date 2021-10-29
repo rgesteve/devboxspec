798eab7761c6a0ee55c5d56c0a92b4c4a1c6550d
R. G. Esteves <rgesteves@gmail.com>
2021-10-29 05:23:27 +0000

    Cosmetic changes and line to delete group

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,7 @@ az login
 # This actually doesn't work because the `list` command returns result with quotes
 az account set --subscription $(az account list | jq '.[0].id')
 
-az group create --name ${RESOURCEGRPNAME} --location "WestUS"
+az group create --name ${RESOURCEGRPNAME} --location "westus"
 az deployment group create --name ${DEPLOYMENTNAME} --resource-group ${RESOURCEGRPNAME} --template-file ubuntu_azvm/first.json 
+
+# az group delete --resource-group ${RESOURCEGRPNAME}
